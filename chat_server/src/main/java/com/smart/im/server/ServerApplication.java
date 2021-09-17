e3aaea69452ca496aa019ad49c1d9c905c63bb79
FrankQian <yangqian3@staff.weibo.com>
2021-09-17 14:08:40 +0800

    init : 基础依赖的引入

---
+++ chat_server/src/main/java/com/smart/im/server/ServerApplication.java
@@ -0,0 +1,20 @@
+package com.smart.im.server;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author frankq
+ * @date 2021/9/17
+ */
+@Slf4j
+@Configuration
+@SpringBootApplication
+public class ServerApplication {
+
+    public static void main(String[] args) {
+
+    }
+
+}
