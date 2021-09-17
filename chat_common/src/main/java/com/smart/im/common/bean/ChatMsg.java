c7b47c8358d0a1252702a682aa09c192ca3d0d60
FrankQian <yangqian3@staff.weibo.com>
2021-09-17 11:12:05 +0800

    init : 通用消息实体，以及模拟用户实体

--- chat_common/src/main/java/com/smart/im/common/bean/ChatMsg.java
+++ chat_common/src/main/java/com/smart/im/common/bean/ChatMsg.java
@@ -1,8 +1,99 @@
 package com.smart.im.common.bean;
 
+import com.smart.im.common.bean.msg.ProtoMsg;
+import lombok.Data;
+import org.apache.commons.lang.StringUtils;
+
 /**
+ *
+ * 消息实体类
+ *
  * @author frankq
  * @date 2021/9/16
  */
+@Data
 public class ChatMsg {
+
+    /**
+     * 消息类型  1：纯文本  2：音频 3：视频 4：地理位置 5：其他
+     */
+    public enum MSGTYPE {
+        TEXT,
+        AUDIO,
+        VIDEO,
+        POS,
+        OTHER;
+    }
+
+    public ChatMsg(User user) {
+        if (null == user) {
+            return;
+        }
+        this.user = user;
+        this.setTime(System.currentTimeMillis());
+        this.setFrom(user.getUid());
+        this.setFromNick(user.getNickName());
+
+    }
+
+    private User user;
+
+    private long msgId;
+    private String from;
+    private String to;
+    private long time;
+    private MSGTYPE msgType;
+    private String content;
+    /**
+     * 多媒体地址
+     */
+    private String url;
+    /**
+     * 附加属性
+     */
+    private String property;
+    /**
+     * 发送者昵称
+     */
+    private String fromNick;
+    /**
+     * 附加的json串
+     */
+    private String json;
+
+
+    public void fillMsg(ProtoMsg.MessageRequest.Builder cb) {
+        if (msgId > 0) {
+            cb.setMsgId(msgId);
+        }
+        if (StringUtils.isNotEmpty(from)) {
+            cb.setFrom(from);
+        }
+        if (StringUtils.isNotEmpty(to)) {
+            cb.setTo(to);
+        }
+        if (time > 0) {
+            cb.setTime(time);
+        }
+        if (msgType != null) {
+            cb.setMsgType(msgType.ordinal());
+        }
+        if (StringUtils.isNotEmpty(content)) {
+            cb.setContent(content);
+        }
+        if (StringUtils.isNotEmpty(url)) {
+            cb.setUrl(url);
+        }
+        if (StringUtils.isNotEmpty(property)) {
+            cb.setProperty(property);
+        }
+        if (StringUtils.isNotEmpty(fromNick)) {
+            cb.setFromNick(fromNick);
+        }
+        if (StringUtils.isNotEmpty(json)) {
+            cb.setJson(json);
+        }
+    }
+
+
 }
--- chat_common/src/main/java/com/smart/im/common/bean/User.java
+++ chat_common/src/main/java/com/smart/im/common/bean/User.java
@@ -1,8 +1,74 @@
 package com.smart.im.common.bean;
 
+import com.smart.im.common.bean.msg.ProtoMsg;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.UUID;
+import java.util.concurrent.atomic.AtomicInteger;
+
 /**
+ * 用户实体
+ * TODO 模拟部分数据
  * @author frankq
  * @date 2021/9/16
  */
+@Slf4j
+@Data
 public class User {
+
+    /**
+     * TODO 用户的ID信息应当是存储在持久化库中
+     */
+    private static final AtomicInteger NO = new AtomicInteger(1);
+
+    String uid = String.valueOf(NO.getAndIncrement());
+    String devId= UUID.randomUUID().toString();
+    String token= UUID.randomUUID().toString();
+    String nickName = "nickName";
+    PLATTYPE platform = PLATTYPE.WINDOWS;
+
+    /**
+     * windows,mac,android,ios,web,other
+     */
+    public enum PLATTYPE {
+        WINDOWS, MAC, ANDROID, IOS, WEB, OTHER;
+    }
+
+    private String sessionId;
+
+
+    public void setPlatform(int platform) {
+        PLATTYPE[] values = PLATTYPE.values();
+        for (int i = 0; i < values.length; i++) {
+            if (values[i].ordinal() == platform) {
+                this.platform = values[i];
+            }
+        }
+
+    }
+
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "uid='" + uid + '\'' +
+                ", devId='" + devId + '\'' +
+                ", token='" + token + '\'' +
+                ", nickName='" + nickName + '\'' +
+                ", platform=" + platform +
+                '}';
+    }
+
+    public static User fromMsg(ProtoMsg.LoginRequest info) {
+        User user = new User();
+        user.uid = new String(info.getUid());
+        user.devId = new String(info.getDeviceId());
+        user.token = new String(info.getToken());
+        user.setPlatform(info.getPlatform());
+        log.info("登录中: {}", user.toString());
+        return user;
+
+    }
+
 }
